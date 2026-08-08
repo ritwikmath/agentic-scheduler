c75d467cb3665126cde21a8cf52eab3ce048ecec
ritwikmath <ritwikmath@gmail.com>
2026-08-09 00:17:59 +0530

    refactored code

--- src/agentic_rag_query_scheduler_poc/job.py
+++ src/agentic_rag_query_scheduler_poc/job.py
@@ -20,21 +20,25 @@ async def worker(queue: asyncio.Queue) -> str:
         finally:
             queue.task_done()
 
+def fetch_pending_queries():
+    conn = sqlite3.connect(DB_PATH)
+    try:
+        cur = conn.cursor()
+        res = cur.execute("SELECT task, id FROM task WHERE schedule_at < datetime('now') and status = 'pending'")
+        rows = res.fetchall()
+        return rows
+    except Exception as ex:
+        print(ex)
+    finally:
+        conn.close()
+
 
 async def main():
     queue = asyncio.Queue()
 
     workers = [asyncio.create_task(worker(queue)) for _ in range(5)]
 
-    conn = sqlite3.connect(DB_PATH)
-
-    cur = conn.cursor()
-
-    res = cur.execute("SELECT task, id FROM task WHERE schedule_at < datetime('now') and status = 'pending'")
-
-    rows = res.fetchall()
-
-    print(len(rows))
+    rows = fetch_pending_queries()
 
     for row in rows:
         await queue.put(row)
